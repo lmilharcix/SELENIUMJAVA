4623a856ad068aac242e4b0b28858b439289b64d
lmilharcix <70980267+lmilharcix@users.noreply.github.com>
2025-08-13 14:57:58 -0400

    Create BaseClass.java

---
+++ BaseClass.java
@@ -0,0 +1,15 @@
+package base;
+
+public class BaseClass {
+
+    static String Authorization_Header_Key;
+
+    public static String getAuthorization_Header_Key() {
+        return Authorization_Header_Key;
+    }
+
+    public static void setAuthorization_Header_Key(String authorization_Header_Key) {
+        Authorization_Header_Key = authorization_Header_Key;
+    }
+
+}
